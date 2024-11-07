8d275b73b3a4b598007458589835fc323c99d87d
devtubercode <devtubercode@gmail.com>
2024-11-06 21:30:07 -0500

    feat: add firebase hosting

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000,
+  },
   envDir: "./",
   resolve: {
     alias: {
